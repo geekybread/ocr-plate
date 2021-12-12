1f9953f54c8e945608b233bf452b94704a781d21
Amol Barwal <79502520+geekybread@users.noreply.github.com>
2021-12-12 18:38:56 +0530

    Update and rename app.py to application.py

--- application.py
+++ application.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request
 import os 
 from deeplearning import OCR
 # webserver gateway interface
-app = Flask(__name__)
+application = Flask(__name__)
 
 BASE_PATH = os.getcwd()
 UPLOAD_PATH = os.path.join(BASE_PATH,'static/upload/')
 
 
-@app.route('/',methods=['POST','GET'])
+@application.route('/',methods=['POST','GET'])
 def index():
     if request.method == 'POST':
         upload_file = request.files['image_name']
@@ -23,4 +23,4 @@ def index():
 
 
 if __name__ =="__main__":
-    app.run(debug=True)
+    application.run(debug=True)
