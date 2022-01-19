6b3d4f24d2ba02ae7b70722c476ada228ef9b377
Amol Barwal <79502520+geekybread@users.noreply.github.com>
2022-01-19 21:34:08 +0530

    Update deeplearning.py

--- deeplearning.py
+++ deeplearning.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import pytesseract as pt
+import os
 
 BASE_PATH = os.getcwd()
-model = tf.keras.models.load_model('./static/models/object_detection.h5')
+model = tf.keras.models.load_model(os.path.join(BASE_PATH,'static/models/object_detection.h5'))
 
 def object_detection(path,filename):
     # read image
